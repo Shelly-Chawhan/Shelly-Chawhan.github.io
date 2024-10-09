3c9bd0d4970b71b359f5808afaf05bf4409e8dfe
Shelly Chawhan <shellychawhan@gmail.com>
2024-10-09 09:38:17 -0400

    another error in contact

--- js/contact.js
+++ js/contact.js
@@ -51,13 +51,13 @@ document.getElementById('contactForm').addEventListener('submit', function(event
 
 function sendMail(name, email, subject, message) {
     let params = {
-        from_email: email, // Set the sender's email
-        name: name,        // Set the sender's name
-        subject: subject,  // Set the email subject
-        message: message,  // Set the email message
+        from_email: email, // Capture the sender's email
+        name: name,        // Capture the sender's name
+        subject: subject,  // Capture the email subject
+        message: message,  // Capture the email message
     };
 
-    // Call the emailjs send function
+    // Call the EmailJS send function
     emailjs.send("service_90o2yup", "template_3bq88ia", params)
     .then(function(response) {
         // Show success message if email was sent successfully
